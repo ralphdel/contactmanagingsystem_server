2efa122502dc924fc678aec0efa7768992176ec4
ralphdel <ralphdel@yahoo.com>
2024-05-28 16:59:42 +0100

    new

--- index.js
+++ index.js
@@ -3,20 +3,30 @@ import dotenv from "dotenv";
 import cors from "cors";
 import "./config/databaseconnection.js";
 import { Router } from "./routes/routes.js";
+dotenv.config({ path: "./config/.env" });
+
 
 const app = express();
 
 app.use(express.json());
 
+const corsOptions = {
+  origin: 'https://ralphcontactms.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use((req, res, next) => {
+  console.log('CORS origin:', req.headers.origin);
+  next();
+});
+
+
 app.use(
-  cors({
-    origin: ["https://ralphcontactms.vercel.app"],
-    methods: ['GET', 'POST', 'PUT','DELETE'],
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
-  })
+  cors((corsOptions))
 );
-dotenv.config({ path: "./config/.env" });
+
 app.use("/contactms", Router);
 
 app.listen(process.env.PORT, () => {
