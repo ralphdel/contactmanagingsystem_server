1d017d29234efc98e10850e2b111d7f765a07d69
ralphdel <ralphdel@yahoo.com>
2024-05-28 16:35:46 +0100

    changes made to cors

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import './config/databaseconnection.js'
-import { Router } from './routes/routes.js'
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import "./config/databaseconnection.js";
+import { Router } from "./routes/routes.js";
 
-const app =express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(cors({
-  origin: ["https://ralphcontactms.vercel.app/"],
-  methods:['GET, POST, PUT, DELETE'],
-  credentials:true
-  
-}))
-dotenv.config({path:"./config/.env"})
-app.use('/contactms', Router)
+app.use(
+  cors({
+    origin: ["https://ralphcontactms.vercel.app"],
+    methods: ['GET', 'POST', 'PUT','DELETE'],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+dotenv.config({ path: "./config/.env" });
+app.use("/contactms", Router);
 
-
-
-
-app.listen(process.env.PORT, ()=>{
-  console.log('server is running')
-})
+app.listen(process.env.PORT, () => {
+  console.log("server is running");
+});
